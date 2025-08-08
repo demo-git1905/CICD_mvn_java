85dbab917c1844bb810c5e083018a9d756d5d1c8
demo-git1905 <piusonawane512@gmail.com>
2025-08-08 14:38:42 +0530

    Update Greeter.java

--- src/main/java/hello/Greeter.java
+++ src/main/java/hello/Greeter.java
@@ -1,7 +1,7 @@
 package hello;
 
 public class Greeter {
-    public String sayHello() {
-        return "DevOps Project for AchiStar Technologies";
+    public static void main(String[] args) {
+        System.out.println("Hello World!")
     }
 }
